fb90f05e151a9b8b2a9a6081b9544fc692e857a2
gowthamXk <gowtham220904@gmail.com>
2025-04-11 20:21:07 +0530

    Time: 98 ms (8.23%), Space: 44.6 MB (65.37%) - LeetHub

---
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+        for(int i = low ; i <= high ; i++ ){
+            if(Integer.toString(i).length() % 2 != 0){
+                continue;
+            }
+            String full = Integer.toString(i);
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int j = 0 ; j < full.length()/2 ; j++ ){
+                sum1 = sum1 + Integer.parseInt(String.valueOf(full.charAt(j)));
+            }
+            for(int k = full.length()/2 ; k < full.length() ; k++){
+                 sum2 = sum2 + Integer.parseInt(String.valueOf(full.charAt(k)));
+            }
+
+            if(sum1 == sum2){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}

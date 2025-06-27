f4a36609dce4943262bb4d620c2c6cb297cf38d1
gowthamXk <gowtham220904@gmail.com>
2025-06-27 15:49:41 +0530

    Time: 24 ms (19.63%), Space: 94.7 MB (59.07%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        HashSet<List<Integer>> set = combo( 1 , n , k , new HashSet<List<Integer>>() , new ArrayList<>());
+        List<List<Integer>> set = combo( 1 , n , k , new ArrayList<List<Integer>>() , new ArrayList<>());
         
-        return new ArrayList<>(set);
+        return set;
     }
-    public HashSet<List<Integer>> combo(int i , int n , int k , HashSet<List<Integer>> sets , List<Integer> li ){
+    public List<List<Integer>> combo(int i , int n , int k , List<List<Integer>> sets , List<Integer> li ){
         if( i > n ){
             if(li.size() == k){
                 sets.add(new ArrayList<Integer>(li));

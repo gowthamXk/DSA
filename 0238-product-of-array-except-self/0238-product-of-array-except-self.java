bdc3fce4a2358eee38192d5c7ed71f4c86e53179
gowthamXk <gowtham220904@gmail.com>
2025-06-28 14:11:58 +0530

    Time: 2 ms (88.71%), Space: 55.4 MB (75.34%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,22 +4,7 @@ class Solution {
         int ans = 0;
         boolean Hasvalue = true;
         int zeroCount = 0;
-        // int[] prefix = new int[nums.length];
-        // int[] suffix = new int[nums.length];
-        // Arrays.fill(prefix,1);
-        // Arrays.fill(suffix,1);
-
-        // for(int i = 1 ; i < nums.length ; i++ ){
-        //     prefix[i] = prefix[i-1]*nums[i-1];
-        // }
-
-        // for(int i = nums.length - 2 ; i >=0 ; i-- ){
-        //     suffix[i] = suffix[i+1]*nums[i+1];
-        // }
         
-        // for(int i = 0 ; i < nums.length ; i++ ){
-        //    answer[i] = prefix[i]*suffix[i];
-        // }
         boolean zero = false;
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] != 0 ){

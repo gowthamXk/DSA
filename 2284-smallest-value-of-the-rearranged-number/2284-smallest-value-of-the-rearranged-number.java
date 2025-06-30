a7d6f634859b682e191806be43d0bd3150de6f8c
gowthamXk <gowtham220904@gmail.com>
2025-06-30 22:05:07 +0530

    Time: 8 ms (13.53%), Space: 41.7 MB (10.63%) - LeetHub

---
+++ 2284-smallest-value-of-the-rearranged-number/2284-smallest-value-of-the-rearranged-number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public long smallestNumber(long num) {
+        ArrayList<Integer> digit = new ArrayList<>();
+        long temp = num;
+        long ans = 0;
+        while(temp != 0){
+            digit.add((int)(temp%10));
+            temp = temp/10;
+        }
+        Collections.sort(digit);
+        System.out.println(digit);
+       
+        if(num > 0){
+        for(int i = 0 ; i < digit.size() ; i++){
+            if(digit.get(i) == 0){
+                continue;
+            }
+            else{
+                long tem = digit.get(0);
+                digit.set(0,digit.get(i));
+                digit.set(i , (int)tem);
+                return sum(digit , 0 , digit.size());
+                
+            }
+        }
+        }else{
+           return -(sum(digit , 0 , digit.size()));
+        }
+        
+        return -1;
+    }
+    public long sum(List<Integer> li , int start , int end){
+        long ans = 0;
+        for(int i = start ; i < end ; i++){
+            if(li.get(i) < 0){
+                li.set(i , -1 * li.get(i));
+            }
+            ans = (ans * 10) + li.get(i);
+        }
+        return ans;
+    }
+}
